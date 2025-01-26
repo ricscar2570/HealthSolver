9793590b4bdac7fb092e75501e557724aaa2fd4e
riccardo <scaringi_riccardo@libero.it>
2025-01-26 19:59:29 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles.css';
+import './i18n';
+
 ReactDOM.render(<App />, document.getElementById('root'));
