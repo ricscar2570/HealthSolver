b4cd29f27565c7d9b4e6891b9a7c3885ffa7d000
riccardo <scaringi_riccardo@libero.it>
2025-04-14 08:35:17 +0200

    Create raw_connection.py
    DB Connection reserved for data engineering

---
+++ backend/raw_connection.py
@@ -0,0 +1,12 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import os
+
+def get_connection():
+    return psycopg2.connect(
+        dbname=os.getenv("POSTGRES_DB", "healthsolver"),
+        user=os.getenv("POSTGRES_USER", "user"),
+        password=os.getenv("POSTGRES_PASSWORD", "password"),
+        host=os.getenv("POSTGRES_HOST", "localhost"),
+        cursor_factory=RealDictCursor
+    )
